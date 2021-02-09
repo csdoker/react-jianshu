2809a0bc0a93d8a2bb182f13da8245cd5f649bbe
csdoker <csd758371536@qq.com>
2021-02-09 13:02:26 +0800

    feat: 引入react路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './common/header'
 import { GlobalStyle } from './style'
 import store from './store'
@@ -9,7 +10,16 @@ class App extends Component {
     return (
       <Fragment>
         <Provider store={store}>
-          <Header />
+          <div>
+            <Header />
+            <Router>
+              <Switch>
+                <Route path='/' exact>
+                  <div>home</div>
+                </Route>
+              </Switch>
+            </Router>
+          </div>
         </Provider>
         <GlobalStyle />
       </Fragment>
