58da743de3568a0758dce9dec7a872050ff9a921
csdoker <csd758371536@qq.com>
2021-02-09 15:35:25 +0800

    feat: 完成首页文章列表区域布局及样式

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -4,10 +4,11 @@ import { TopicWrapper, TopicItem } from '../style'
 
 class Topic extends Component {
   render () {
+    const { topicList } = this.props
     return (
       <TopicWrapper>
         {
-          this.props.topicList.map(item => {
+          topicList.map(item => {
             return (
               <TopicItem key={item.get('id')}>
                 <img className='topic-pic' src={item.get('imgUrl')} alt='' />
