5b579e083f4fe10b5a0c5e330c081971c418828b
csdoker <csd758371536@qq.com>
2021-02-10 16:44:51 +0800

    feat: 异步组件及withRouter路由方法的使用

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
+// import { withRouter } from 'react-router-dom'
 import { actionCreators } from './store'
 import { DetailWrapper, Header, Content } from './style'
 
